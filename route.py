6f4033d0da2378fea4762d081516456698ec6b5c
hekarmannnn12 <hehetester04@gmail.com>
2025-07-15 07:23:57 +0545

    Create route.py

---
+++ route.py
@@ -0,0 +1,12 @@
+# fake_secrets.py
+
+# 🚨 These are **fake** keys for testing secret detection scripts.
+
+AWS_SECRET_KEY = "AKIAIOSFODNN7EXAMPLE"
+GOOGLE_API_KEY = "AIzaSyD4H-FakeGoogleAPIKey123456789012345678901"
+SLACK_TOKEN = "xoxb-123456789012-abcdef1234567890abcdef"
+HEROKU_API_KEY = "heroku1234567890abcdef1234567890ab"
+PRIVATE_KEY = """
+-----BEGIN PRIVATE KEY-----
+MIIBVgIBADANBgkqhkiG9w0BAQEFAASCAT8wggE7AgEAAkEAzQ0/fakekeyexample
+-----END
